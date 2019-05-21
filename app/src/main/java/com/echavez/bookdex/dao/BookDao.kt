b0d7f6ed0193b8730bc266a195a5a96d3f19ddb7
Fredy Sanchez <00082817@uca.edu.sv>
2019-05-20 23:13:01 -0600

    Busqueda de libro

--- app/src/main/java/com/echavez/bookdex/dao/BookDao.kt
+++ app/src/main/java/com/echavez/bookdex/dao/BookDao.kt
@@ -5,24 +5,40 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import com.echavez.bookdex.entities.Author
 import com.echavez.bookdex.entities.Book
+import com.echavez.bookdex.entities.Tag
 
 @Dao
 interface BookDao {
-    @Query("SELECT * from book_table WHERE isbn= :isbn")
-    fun getBook(isbn: String): LiveData<Book>
 
-    /*
+    //GET
     @Query("SELECT * from book_table WHERE isbn= :isbn")
-    fun getBookByISBN(isbn: String): Book
-    */
+    fun getBookByIsbn(isbn: String): LiveData<Book>
 
+    @Query("SELECT * from book_table WHERE title= :title")
+    fun getBookByTitle(title: String): Book
+
+    @Query("SELECT * from book_table WHERE editorial= :editorial")
+    fun getBookByEditorial(editorial: String): Book
+
+    @Query("SELECT * from book_table WHERE authors= :authors")
+    fun getBookByAuthors(authors: List<Author>): Book
+
+    @Query("SELECT * from book_table WHERE tags= :tags")
+    fun getBookByTag(tags: List<Tag>): Book
+
+
+
+    //Get all
     @Query("SELECT * from book_table ORDER BY isbn ASC")
     fun getAllBooks(): LiveData<List<Book>>
 
+    //Insert
     @Insert
     suspend fun insertBook(book: Book)
 
+    //Check or unchek as favourite
     @Query("UPDATE book_table SET favourite = :preference WHERE isbn = :isbn")
     fun favourite(isbn: String, preference: Boolean)
 
