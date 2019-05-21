0470473dc02950009165812ff3d6bc248382a69a
Elsy Chavez <00125717@gmail.com>
2019-05-20 21:04:01 -0600

    Initial commit

---
+++ app/src/main/java/com/echavez/bookdex/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.echavez.bookdex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //let's do this :'v
+    }
+}
