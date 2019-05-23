965480e2004d6ba02cf298107d60c4ee008546f5
Elsy Chavez <00125717@gmail.com>
2019-05-23 16:48:02 -0600

    funcion init para meter valores de libro, sigue en proceso

--- app/src/main/java/com/echavez/bookdex/Adapter/BookAdapter.kt
+++ app/src/main/java/com/echavez/bookdex/Adapter/BookAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ExpandableListView
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 //import com.bumptech.glide.Glide
 //import com.bumptech.glide.Glide
 import com.echavez.bookdex.R
@@ -14,8 +15,8 @@ import com.echavez.bookdex.ViewModel.BookViewModel
 import com.echavez.bookdex.entities.Book
 import kotlinx.android.synthetic.main.cardview_libro.view.*
 
-class BooksAdapter internal  constructor(context: Context, val clickListener: (Book) -> Unit ) : RecyclerView.Adapter<BooksAdapter.ViewHolder>() {
-     private var books: List<Book> = emptyList()
+class BooksAdapter internal constructor(context: Context, val clickListener: (Book) -> Unit) : RecyclerView.Adapter<BooksAdapter.ViewHolder>() {
+    private var books: List<Book> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.cardview_libro, parent, false)
@@ -34,10 +35,10 @@ class BooksAdapter internal  constructor(context: Context, val clickListener: (B
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bind(book: Book, clickListener: (Book) -> Unit) = with(itemView) {
-           // Glide.with(itemView.context)
-             //   .load(book.cover)
-               // .placeholder(R.drawable.ic_launcher_background)
-                //.into(Iv_book_portada)
+            Glide.with(itemView.context)
+                    .load(book.cover)
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .into(Iv_book_portada)
             Tv_book_name.text = book.title
             setFavorite.setOnClickListener {
                 clickListener(book)
--- app/src/main/java/com/echavez/bookdex/activities/BookViewerActivity.kt
+++ app/src/main/java/com/echavez/bookdex/activities/BookViewerActivity.kt
@@ -2,7 +2,10 @@ package com.echavez.bookdex.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.echavez.bookdex.R
+import com.echavez.bookdex.entities.Book
+import kotlinx.android.synthetic.main.activity_book_viewer.*
 
 //vista interna, detalle del libro proximamente
 class BookViewerActivity : AppCompatActivity() {
@@ -10,5 +13,18 @@ class BookViewerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_viewer)
+    }
+
+    fun init(book: Book){
+        Glide.with(this)
+                .load(book.cover)
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(Iv_book_cover)
+        tv_book_namae.text = book.title
+        tv_author_name.text = book.author.toString()
+        tv_book_edicion.text = book.edition
+        tv_book_editorial.text = book.editorial
+        tv_book_isbn.text = book.isbn
+        tv_book_resumen.text = book.summary
     }
 }
