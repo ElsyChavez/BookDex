8b4345a1501da0f2f8311e98af19b4dae66f4d22
GuillermoMorales <00011417@uca.edu.sv>
2019-05-23 17:37:46 -0600

    Agregando registro BD

--- app/src/main/java/com/echavez/bookdex/database/BookDexDatabase.kt
+++ app/src/main/java/com/echavez/bookdex/database/BookDexDatabase.kt
@@ -73,12 +73,30 @@ public abstract class BookDexDatabase : RoomDatabase() {
             authorDao.insertAuthor(aut)
             aut = Author(4, "Dante", "Alighieri")
             authorDao.insertAuthor(aut)
+            aut = Author(5,"Homero","")
+            authorDao.insertAuthor(aut)
+            aut = Author(6,"Dan","Brown")
+            authorDao.insertAuthor(aut)
+            aut = Author(7,"Robert","Kiyosaki")
+            authorDao.insertAuthor(aut)
+            aut = Author(8,"Sun","Tzu")
+            authorDao.insertAuthor(aut)
+            aut = Author(9,"Jordan","Belfort")
+            authorDao.insertAuthor(aut)
 
             var tag = Tag( 1,"Ficcion" )
             tagDao.insertTag(tag)
             tag= Tag(2, "Politica" )
             tagDao.insertTag(tag)
             tag= Tag(3,"Epopeya")
+            tagDao.insertTag(tag)
+            tag = Tag(4,"Novela")
+            tagDao.insertTag(tag)
+            tag = Tag(5,"Economia")
+            tagDao.insertTag(tag)
+            tag = Tag(6,"Ficcción")
+            tagDao.insertTag(tag)
+
 
             var book = Book("4213432324", "https://www.abc.es/media/familia/2018/04/06/PORTADA-ELPRINCIPITO-kwpB--620x349@abc.JPG", "El principito", "1ra", "El castillo", "Cuenta la hisotira de un pequeño niño que..", 1,1, true)
             bookDao.insertBook(book)
@@ -89,8 +107,17 @@ public abstract class BookDexDatabase : RoomDatabase() {
             book = Book("6548523432", "https://imagessl3.casadellibro.com/a/l/t5/33/9788498890433.jpg", "Espada del destino", "1ra", "Casa de timbre", "breves que preceden la serie principal de Geralt de Rivia", 3,1, false)
             bookDao.insertBook(book)
             book = Book("9789500398152","https://images-na.ssl-images-amazon.com/images/I/510SxGcsw-L._SX331_BO1,204,203,200_.jpg", "La Divina Comedia","1ra","LOSADA S.A., EDITORIAL","La travesia de Dante por los 9 circulos del infierno",4,3,true)
-
-
+            bookDao.insertBook(book)
+            book = Book("1502735180","https://i.ebayimg.com/images/g/jVYAAOSw4A5YqQGU/s-l500.jpg","La Iliada","1ra","La Biblioteca digital","Es una epopeya griega, atribuida tradicionalmente a Homero",5,3,false)
+            bookDao.insertBook(book)
+            book = Book("9788071459316","https://imagessl8.casadellibro.com/a/l/t5/08/9788408176008.jpg","Angeles y Demonios","1ra","Planeta","El mayor enemigo de la iglesia amenaza con destruirla desde sus cimientos",6,4,false)
+            bookDao.insertBook(book)
+            book = Book("9788466332125","https://imagessl5.casadellibro.com/a/l/t5/25/9788466332125.jpg","Padre rico padre pobre","1ra","Debolsillo (Punto de lectura)","Libro de finanzas personales número 1 en todo el mundo",7,5,true)
+            bookDao.insertBook(book)
+            book = Book("9781590302255","https://images-na.ssl-images-amazon.com/images/I/51YhPgNl8UL._SX302_BO1,204,203,200_.jpg","El arte de la guerra","1ra","Shambhala","El conflicto es parte de la vida que no se puede evitar",8,6,false)
+            bookDao.insertBook(book)
+            book = Book("0340953756","https://pictures.abebooks.com/isbn/9780340953754-es.jpg","El lobo de Wallstreet","1ra","Hodder Paperback","Es un libro de memorias del excorredor de bolsa estadounidense Jordan Belfort",9,5,true)
+            bookDao.insertBook(book)
         }
     }
 }
